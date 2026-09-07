ed273d3a2aacd7e86d0bc8facdf4b1ab1e39bb4d
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-07 15:31:18 +0530

    Update solution.sh

--- starter/solution.sh
+++ starter/solution.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # Assignment:
-
 # Add a Linux user and display the details of the user.
 
 USERNAME="studentuser"
 
-# TODO 1:
+# TODO 1: Create the user
+useradd "$USERNAME"
 
-# Create the user using the useradd command.
+# TODO 2: Display the details of the user
+id "$USERNAME"
 
-# TODO 2:
-
-# Display the details of the user using the id command.
-
-# TODO 3:
-
-# Exit successfully.
+# TODO 3: Exit successfully
+exit 0
